5fdda0e4fb37c19b52dc028550218235b2405e94
joshjames123 <bueranoj@gmail.com>
2022-05-20 14:12:15 +0800

    initial setup

---
+++ app.js
@@ -0,0 +1,31 @@
+//[SECTION] Dependencies and Modules
+	const express = require('express');
+	const mongoose = require('mongoose');
+	const dotenv = require('dotenv');
+	const userRoutes = require('./routes/users')
+
+	//[SECTION] Environment Setup
+	dotenv.config();
+	let account = process.env.CREDENTIALS;
+	const port = process.env.PORT;
+
+	//[SECTION] Server Setup
+	const app = express();
+	app.use(express.json());
+
+	//[SECTION] Database Connection
+	mongoose.connect(account)
+	const connectStatus = mongoose.connection
+	connectStatus.once('open', () => console.log(`Database Connected`));
+
+	//[SECTION] Backend Routes
+	app.use('/users', userRoutes);
+
+	//[SECTION] Server Gateway Respose
+	app.get('/', (req, res) => {
+		res.send('Welcome to Ecommers Application')
+	});
+
+	app.listen(port, () => {
+		console.log(`API is Hosted port ${port}`);
+	});
