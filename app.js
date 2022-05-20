9aa34544393c11cba07ff75e387269e600e480e9
joshjames123 <bueranoj@gmail.com>
2022-05-20 20:22:50 +0800

    update for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 	const express = require('express');
 	const mongoose = require('mongoose');
 	const dotenv = require('dotenv');
-	const userRoutes = require('./routes/users');
+	//const userRoutes = require('./routes/users');
 
 //[SECTION] Environment Setup
 	dotenv.config();
@@ -19,11 +19,11 @@
 	connectStatus.once('open', () => console.log(`Database Connected`));
 
 //[SECTION] Backend Routes
-	appj.use('/users', userRoutes);
+	//appj.use('/users', userRoutes);
 
 //[SECTION] Server Gateway Respose
 	appj.get('/', (req, res) => {
-		res.send('Welcome to Ecommers JBro')
+		res.send('Welcome to Ecommers JJBro')
 	});
 
 	appj.listen(port, () => {
