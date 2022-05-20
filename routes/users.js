6858d5d987db354f4fdc7a7f409875275b3194e7
joshjames123 <bueranoj@gmail.com>
2022-05-20 15:47:13 +0800

    done with routes setup

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+//[SECTIONS] Dependencies and Modules
+	const exp = require('express');
+	const controller = require('../controller/users');
+
+//[SECTIONS] Routing Component
+	const route = exp.Router();
+
+//[SECTIONS] Routes- POST
+	route.post('/register', (req, res) => {
+		console.log(req.body);
+		let userData = req.body;
+		
+		controller.register(userData).then(outCome => {
+			res.send(outCome)
+		})
+	});
+
+//[SECTIONS] Expose Route System
+	module.exports = route;
