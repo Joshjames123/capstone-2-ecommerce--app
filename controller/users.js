1456c5b3937ca737d8f717637dac3913e44de8f4
joshjames123 <bueranoj@gmail.com>
2022-05-20 15:26:55 +0800

    done with controller setup

---
+++ controller/users.js
@@ -0,0 +1,32 @@
+//[SECTIONS] Dependencies and Modules
+	const User = require('../models/User')
+	const bcrypt = require('bcrypt');
+	const dotenv = require('dotenv');
+
+//[SECTION] Environment Variables Setup
+	dotenv.config()
+	const salt = Number(process.env.SALT);
+
+
+//[SECTIONS] Functionalities [CREATE]
+	//1. Register New Account
+	module.exports.register = (userData) => {
+		let email = userData.email;
+		let passW = userData.password;
+		let isAd = userData.isAdmin;
+
+		let newUser = new User({
+			email: email,
+			password: bcrypt.hashSync(passW, salt),
+			isAdmin: isAd
+		})
+		
+		return newUser.save().then((user, err) => {
+			
+			if (user) {
+				return user;
+			} else {
+				return 'Failed to Register account';
+			}
+		});
+	};
