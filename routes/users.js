71daaec160437a44214f9c0519ab4c4fdba7d549
joshjames123 <bueranoj@gmail.com>
2022-05-20 16:15:00 +0800

    done with models and user-register setup

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@
 
 //[SECTIONS] Routes- POST
 	route.post('/register', (req, res) => {
-		console.log(req.body);
+		//console.log(req.body);
 		let userData = req.body;
 		
 		controller.register(userData).then(outCome => {
